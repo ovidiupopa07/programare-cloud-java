24133ccd1beb80976225639d5534b7e51382baaa
Stefan Petrescu <stefan_petrescu007@yahoo.ro>
2022-03-02 07:58:38 +0200

    refactor

--- eureka/persons-server/src/main/java/com/eureka/persons/base/AbstractEntity.java
+++ eureka/persons-server/src/main/java/com/eureka/persons/base/AbstractEntity.java
@@ -1,34 +1,9 @@
-/*
-Freeware License, some rights reserved
-
-Copyright (c) 2019 Iuliana Cosmina
-
-Permission is hereby granted, free of charge, to anyone obtaining a copy 
-of this software and associated documentation files (the "Software"), 
-to work with the Software within the limits of freeware distribution and fair use. 
-This includes the rights to use, copy, and modify the Software for personal use. 
-Users are also allowed and encouraged to submit corrections and modifications 
-to the Software for the benefit of other users.
-
-It is not allowed to reuse,  modify, or redistribute the Software for 
-commercial use in any way, or for a user's educational materials such as books 
-or blog articles without prior permission from the copyright holder. 
-
-The above copyright notice and this permission notice need to be included 
-in all copies or substantial portions of the software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS OR APRESS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
 package com.eureka.persons.base;
 
 import com.eureka.persons.util.DateProcessor;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
@@ -38,6 +13,8 @@ import java.util.Comparator;
 import java.util.Objects;
 
 @MappedSuperclass
+@Getter
+@Setter
 public abstract class AbstractEntity implements Serializable {
 
     public static Comparator<AbstractEntity> COMPARATOR_BY_ID = Comparator.comparing(AbstractEntity::getId);
@@ -67,44 +44,6 @@ public abstract class AbstractEntity implements Serializable {
     protected AbstractEntity() {
         createdAt = LocalDateTime.now();
         modifiedAt = LocalDateTime.now();
-    }
-
-    /**
-     * Returns the entity identifier. This identifier is unique per entity. It is used by persistence frameworks used in a project,
-     * and although is public, it should not be used by application code.
-     * This identifier is mapped by ORM (Object Relational Mapper) to the database primary key of the Person record to which
-     * the entity instance is mapped.
-     *
-     * @return the unique entity identifier
-     */
-    public Long getId() {
-        return id;
-    }
-
-    /**
-     * Sets the entity identifier. This identifier is unique per entity.  Is is used by persistence frameworks
-     * and although is public, it should never be set by application code.
-     *
-     * @param id the unique entity identifier
-     */
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getModifiedAt() {
-        return modifiedAt;
-    }
-
-    public void setModifiedAt(LocalDateTime modifiedAt) {
-        this.modifiedAt = modifiedAt;
     }
 
     // IDE generated methods
