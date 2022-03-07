a5b48855046d4aac1900841399e3338cb7497947
PetreMihaiBogdan <petrebogdan55@yahoo.coom>
2022-03-07 16:28:55 +0200

    PCJ Tema 2 - Teste Spring1

--- spring1/src/main/java/com/unitbv/beans/Person.java
+++ spring1/src/main/java/com/unitbv/beans/Person.java
@@ -1,8 +1,18 @@
 package com.unitbv.beans;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Person implements Human {
 
+    @Autowired
     private Item item;
+
+    public Person(Item item)
+    {
+        this.item=item;
+    }
 
     @Override
     public Item getItem() {
