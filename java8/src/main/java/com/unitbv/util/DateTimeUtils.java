6ca626381c4c3adc4f1ab29c1a7d1a2b113a24ad
Adrian Calavie <adrian.calavie06@gmail.com>
2022-03-11 00:34:14 +0200

    Resolved issues in DateTimeUtils and UserDataSource

--- java8/src/main/java/com/unitbv/util/DateTimeUtils.java
+++ java8/src/main/java/com/unitbv/util/DateTimeUtils.java
@@ -2,18 +2,22 @@ package com.unitbv.util;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.temporal.ChronoField;
 
 public class DateTimeUtils {
-
+    
     // Get the number of years until the given date
-    public static int getNumberOfYearsUntil(LocalDate date){
-        // your code here
-        return 0;
+    public static int getNumberOfYearsUntil(LocalDate date) {
+        var now = LocalDate.now();
+        return date.getYear() - now.getYear();
     }
-
+    
     // Check if the given date occurs on Friday the 13th
-    public static boolean isDateOccurringOnFriday13th(LocalDate date){
+    public static boolean isDateOccurringOnFriday13th(LocalDate date) {
         // your code here - HINT: use ChronoField enum constants for day of month and day of week
-        return false;
+        var friday = ChronoField.DAY_OF_WEEK.checkValidIntValue(5);
+        var thirteen = ChronoField.DAY_OF_MONTH.checkValidIntValue(13);
+        
+        return date.getDayOfWeek().getValue() == friday && date.getDayOfMonth() == thirteen;
     }
 }
