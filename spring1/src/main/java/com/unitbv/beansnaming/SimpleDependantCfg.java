473cc0680ad4da60f58dbfbf7e2df6e95599ada2
Bogdan Pavel <bogdan.pavel@tadano.com>
2022-03-11 11:25:36 +0200

    Tema1 ,Tema 2

--- spring1/src/main/java/com/unitbv/beansnaming/SimpleDependantCfg.java
+++ spring1/src/main/java/com/unitbv/beansnaming/SimpleDependantCfg.java
@@ -2,8 +2,12 @@ package com.unitbv.beansnaming;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Configuration
 @ComponentScan(basePackages = {"com.unitbv.beansnaming"})
@@ -11,10 +15,15 @@ public class SimpleDependantCfg {
 
     private final Logger logger = LoggerFactory.getLogger(SimpleDependantCfg.class);
 
+    @Bean(name = {"customSimpleBean", "customSimpleBean1", "customSimpleBean2"} )
+//    @Bean
+    //@Qualifier("customSimpleBean")
     SimpleBean simpleBean() {
         return new SimpleBeanImpl();
     }
 
+    @Bean(name = {"customDependantBean", "customDependantBean1", "customDependantBean2"} )
+//    @Bean
     DependantBean dependantBean() {
         return new DependantBeanImpl(simpleBean());
     }
