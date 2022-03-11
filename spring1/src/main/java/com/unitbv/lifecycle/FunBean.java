03b578f4a7991a6f55a082f1e373c52beba50f21
Ana0609 <85016977+Ana0609@users.noreply.github.com>
2022-03-11 10:07:38 +0200

    lab1+lab2

--- spring1/src/main/java/com/unitbv/lifecycle/FunBean.java
+++ spring1/src/main/java/com/unitbv/lifecycle/FunBean.java
@@ -13,4 +13,21 @@ public class FunBean {
     private Logger logger = LoggerFactory.getLogger(FunBean.class);
 
     private DepBean depBean;
+
+    @Autowired
+    public void setDepBean(DepBean depBean) {
+        this.depBean = depBean;
+        System.out.println("setter");
+    }
+    void test(){
+        System.out.println("test");
+    }
+
+    public FunBean() {
+        System.out.println("constructor");
+    }
+
+    void testDestroy(){
+        System.out.println("destroy");
+    }
 }
--- spring1/src/main/java/com/unitbv/stereotype/StereotypeCfg.java
+++ spring1/src/main/java/com/unitbv/stereotype/StereotypeCfg.java
@@ -1,9 +1,15 @@
 package com.unitbv.stereotype;
 
+import com.unitbv.stereotype.controller.UserController;
+import com.unitbv.stereotype.repository.UserRepository;
+import com.unitbv.stereotype.service.UserService;
+import com.unitbv.stereotype.util.UserUtil;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "com.unitbv")
+@ComponentScan(basePackages="com.unitbv.stereotype")
+
 public class StereotypeCfg {
 }
