46d1634222599d011c7be50f29f12af9a88d7f27
Florin <florinstroe2020@gmail.com>
2022-03-11 10:48:54 +0200

    Tema 2 Stroe Florin

--- spring1/src/main/java/com/unitbv/beans/Person.java
+++ spring1/src/main/java/com/unitbv/beans/Person.java
@@ -1,8 +1,17 @@
 package com.unitbv.beans;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Person implements Human {
 
     private Item item;
+
+    @Autowired
+    public Person(Item item) {
+        this.item = item;
+    }
 
     @Override
     public Item getItem() {
