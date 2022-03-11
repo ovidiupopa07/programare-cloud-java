5206bf2148d23b92959f59a4c333f4c84d637a51
TecuAndreea <tecu_andreea@yahoo.com>
2022-03-11 10:39:46 +0200

    Lab 1 si lab 2 - Tecu Andreea Nicoleta

--- spring1/src/main/java/com/unitbv/stereotype/service/impl/UserServiceImpl.java
+++ spring1/src/main/java/com/unitbv/stereotype/service/impl/UserServiceImpl.java
@@ -1,6 +1,9 @@
 package com.unitbv.stereotype.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.unitbv.stereotype.service.UserService;
 
+@Service
 public class UserServiceImpl implements UserService {
 }
