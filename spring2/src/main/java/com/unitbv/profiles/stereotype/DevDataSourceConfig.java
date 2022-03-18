22f8c09b1a9d891163bdb4d5c178162624d06ee0
Radu <alexx15raduu@gmail.com>
2022-03-18 10:10:38 +0200

    Lab 3. Radu Alexandru

--- spring2/src/main/java/com/unitbv/profiles/stereotype/DevDataSourceConfig.java
+++ spring2/src/main/java/com/unitbv/profiles/stereotype/DevDataSourceConfig.java
@@ -1,5 +1,11 @@
 package com.unitbv.profiles.stereotype;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("dev")
 public class DevDataSourceConfig implements DataSourceConfig {
     @Override
     public void setup() {
