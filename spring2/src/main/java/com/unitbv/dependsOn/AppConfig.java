a44bfc4efc082d26a3769bbdaa5f70117bea81ce
PetriucEduardGeorge <eduard.petriuc@ecenta.com>
2022-03-18 14:05:45 +0200

    tema 3 - Petriuc Eduard-George

--- spring2/src/main/java/com/unitbv/dependsOn/AppConfig.java
+++ spring2/src/main/java/com/unitbv/dependsOn/AppConfig.java
@@ -1,8 +1,28 @@
 package com.unitbv.dependsOn;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 
 @Configuration
+@ComponentScan("com.unitbv.dependsOn")
 public class AppConfig {
 
+    @Bean
+    @DependsOn("beanB")
+    public BeanA beana(){
+        return new BeanA();
+    }
+
+    @Bean
+    @DependsOn("beanC")
+    public BeanB beanB(){
+        return new BeanB();
+    }
+
+    @Bean
+    public BeanC beanC(){
+        return new BeanC();
+    }
 }
--- spring2/src/main/java/com/unitbv/profiles/automatically/DevDataSourceConfig.java
+++ spring2/src/main/java/com/unitbv/profiles/automatically/DevDataSourceConfig.java
@@ -1,5 +1,6 @@
 package com.unitbv.profiles.automatically;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
