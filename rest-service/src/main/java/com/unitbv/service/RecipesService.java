0341a81c5f3a3f73a3cf6efbfa452a5fd93af9c1
andwow <53525076+andwow@users.noreply.github.com>
2022-03-31 22:36:32 +0300

    tema rest-service

---
+++ rest-service/src/main/java/com/unitbv/service/RecipesService.java
@@ -0,0 +1,11 @@
+package com.unitbv.service;
+
+
+import com.unitbv.request.CreateRecipeRequest;
+
+import java.util.List;
+
+public interface RecipesService {
+    CreateRecipeRequest saveRecipe(CreateRecipeRequest recipe);
+    List<CreateRecipeRequest> getAllRecipes();
+}
