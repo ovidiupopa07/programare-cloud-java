8890e95c2b0aeff8ad01df5226e53870d11ad5ee
tireacosmin <cosmin.tirea@gmail.com>
2022-04-01 11:33:48 +0300

    lab 4 Tirea Cosmin

--- rest-service/src/main/java/com/unitbv/service/PantryService.java
+++ rest-service/src/main/java/com/unitbv/service/PantryService.java
@@ -5,5 +5,8 @@ import com.unitbv.model.Ingredient;
 import java.util.List;
 
 public interface PantryService {
+    Ingredient saveIngredient(Ingredient ingredient);
+    Ingredient updateIngredient(Ingredient ingredient);
+    boolean deleteIngredient(String name);
     List<Ingredient> getAllIngredients();
 }
