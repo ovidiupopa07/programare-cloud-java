587f43a8f8a448d1fcedd6eefdc6a19c42b550a6
Radu <alexx15raduu@gmail.com>
2022-04-07 17:46:15 +0300

    Tema 5 Radu Alexandru

--- eureka/persons-server/src/main/java/com/eureka/persons/PersonsController.java
+++ eureka/persons-server/src/main/java/com/eureka/persons/PersonsController.java
@@ -1,5 +1,6 @@
 package com.eureka.persons;
 
+import com.eureka.persons.ex.NotFoundException;
 import com.eureka.persons.person.Person;
 import com.eureka.persons.services.PersonService;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 @RestController
@@ -26,7 +29,17 @@ public class PersonsController {
     @ResponseStatus(HttpStatus.OK)
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Person> list() {
-        return new ArrayList<>();
+        List<Person> list = personService.findAll();
+
+        //sort by id
+        list.sort(new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.getId().compareTo(o2.getId());
+            }
+        });
+
+        return list;
     }
 
     /**
@@ -36,6 +49,11 @@ public class PersonsController {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public void create(@RequestBody Person person, BindingResult result) {
+        if(result.hasErrors()){
+            throw new PersonsException(HttpStatus.BAD_REQUEST, "Something went wrong!");
+        } else{
+            personService.save(person);
+        }
     }
 
     /**
@@ -48,7 +66,7 @@ public class PersonsController {
     @ResponseStatus(HttpStatus.OK)
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Person show(@PathVariable Long id) {
-        return new Person();
+        return personService.findById(id).orElseThrow(() -> new NotFoundException(Person.class, id));
     }
 
     /**
@@ -62,6 +80,17 @@ public class PersonsController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
     public void update(@RequestBody Person updatedPerson, @PathVariable Long id) {
+
+        Person person = personService.findById(id).orElseThrow(() -> new NotFoundException(Person.class, id));
+
+        person.setUsername(updatedPerson.getUsername());
+        person.setFirstName(updatedPerson.getFirstName());
+        person.setLastName(updatedPerson.getLastName());
+        person.setPassword(updatedPerson.getPassword());
+        person.setHiringDate(updatedPerson.getHiringDate());
+        person.setNewPassword(updatedPerson.getNewPassword());
+
+        personService.save(person);
     }
 
     /**
@@ -73,5 +102,7 @@ public class PersonsController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
+
+        personService.delete(personService.findById(id).orElseThrow(() -> new NotFoundException(Person.class, id)));
     }
 }
