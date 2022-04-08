b633af20dae611be47c8f911c01a32643bc36f4b
andwow <53525076+andwow@users.noreply.github.com>
2022-04-08 11:14:02 +0300

    tema eureka - Timus Andrei-Flavian

--- eureka/persons-server/src/main/java/com/eureka/persons/PersonsServer.java
+++ eureka/persons-server/src/main/java/com/eureka/persons/PersonsServer.java
@@ -5,9 +5,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 import java.io.IOException;
 
+@EnableEurekaServer
 @EntityScan(basePackages = "com.eureka.persons")
 @SpringBootApplication
 public class PersonsServer {
