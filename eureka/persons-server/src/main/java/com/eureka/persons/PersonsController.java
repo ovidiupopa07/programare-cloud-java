d2cb16f8b0ea3c4bdb19063af5c4c5917b5831f5
Alex N <alexneacsu2000@gmail.com>
2022-04-10 18:38:55 +0300

    Added Homework 5.

--- eureka/persons-server/src/main/java/com/eureka/persons/PersonsController.java
+++ eureka/persons-server/src/main/java/com/eureka/persons/PersonsController.java
@@ -1,5 +1,6 @@
 package com.eureka.persons;
 
+import com.eureka.persons.ex.NotFoundException;
 import com.eureka.persons.person.Person;
 import com.eureka.persons.services.PersonService;
 import org.springframework.http.HttpStatus;
@@ -26,7 +27,8 @@ public class PersonsController {
     @ResponseStatus(HttpStatus.OK)
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Person> list() {
-        return new ArrayList<>();
+
+        return personService.findAll();
     }
 
     /**
@@ -36,6 +38,10 @@ public class PersonsController {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public void create(@RequestBody Person person, BindingResult result) {
+        if(result.hasErrors()){
+            throw new PersonsException(HttpStatus.BAD_REQUEST,"The person couldn't be saved.");
+        }
+        personService.save(person);
     }
 
     /**
@@ -48,7 +54,7 @@ public class PersonsController {
     @ResponseStatus(HttpStatus.OK)
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Person show(@PathVariable Long id) {
-        return new Person();
+        return personService.findById(id).orElseThrow(()-> new NotFoundException(Person.class,id));
     }
 
     /**
@@ -62,6 +68,14 @@ public class PersonsController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
     public void update(@RequestBody Person updatedPerson, @PathVariable Long id) {
+        Person person=personService.findById(id).orElseThrow(()-> new NotFoundException(Person.class,id));
+        person.setFirstName(updatedPerson.getFirstName());
+        person.setLastName(updatedPerson.getLastName());
+        person.setUsername(updatedPerson.getUsername());
+        person.setPassword(updatedPerson.getPassword());
+        person.setHiringDate(updatedPerson.getHiringDate());
+        person.setNewPassword(updatedPerson.getNewPassword());
+        personService.save(person);
     }
 
     /**
@@ -73,5 +87,6 @@ public class PersonsController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
+        personService.delete(personService.findById(id).orElseThrow(()-> new NotFoundException(Person.class,id)));
     }
 }
