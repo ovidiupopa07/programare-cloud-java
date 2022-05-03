18696b57d40837a8f6a52ed29fc8fa1a1a55b9db
Andronache Alexandru Victor <andronachevictor@yahoo.com>
2022-05-03 18:40:38 +0300

    tema 7

---
+++ lab-6-api-gateway/service1/src/main/java/com/example/service1/Controller.java
@@ -0,0 +1,16 @@
+package com.example.service1;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+    @GetMapping("/greeting/{name}")
+    public String greeting(@PathVariable String name) {
+        System.out.println(name);
+        return "Hello "+name;
+    }
+}
